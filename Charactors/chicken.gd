7fa485618a9967edee92a410a2ca3a2f83a3dafc
daichijocky <thomasdwilliford@gmail.com>
2025-05-24 23:52:22 -0700

    hi cutie

--- Charactors/chicken.gd
+++ Charactors/chicken.gd
@@ -3,3 +3,42 @@ extends CharacterBody2D
 enum CHICKEN_STATE { IDLE, WALK }
 
 @export var move_speed : float = 27 
+@export var idle_time : float = 5
+@export var walk_time : float = 2
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+@onready var sprite = $Sprite2D
+@onready var timer = $Timer
+
+var move_direction : Vector2 = Vector2.ZERO
+var current_state : CHICKEN_STATE = CHICKEN_STATE.IDLE
+
+func _ready():
+	select_new_direction()
+
+func _phiysics_process(_delta):
+	velocity = move_direction * move_speed
+	
+	move_and_slide()
+
+
+func select_new_direction():
+	move_direction = Vector2(
+		randi_range(-1,1),
+		randi_range(-1,1)
+	)
+
+func pick_new_state():
+	if(current_state == CHICKEN_STATE.IDLE):
+		state_machine.travel("walk")
+		current_state = CHICKEN_STATE.WALK
+		timer.start(walk_time)
+	elif (current_state == CHICKEN_STATE.WALK):
+		state_machine.travel("Idle")
+		current_state = CHICKEN_STATE.IDLE
+		timer.start(idle_time)
+
+
+func _on_timer_timeout() -> void:
+	pick_new_state() # Replace with function body.
