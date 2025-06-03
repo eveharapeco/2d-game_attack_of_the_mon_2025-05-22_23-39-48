7bfee24ac633c96cd329abb97d0ddcbdc96d2ebe
daichijocky <thomasdwilliford@gmail.com>
2025-06-02 21:31:21 -0700

    fff

---
+++ main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/game_level.tscn")
+
+
+func _on_options_pressed() -> void:
+	print()
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
